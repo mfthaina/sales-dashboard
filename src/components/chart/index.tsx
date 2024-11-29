4ad0af39b751fe80be3903339303c9f110a249f0
Thainá Souza <mfthaina.souza@gmail.com>
2024-11-29 19:50:58 -0300

    Chart update

--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -9,9 +9,9 @@ const ChartOverview = () => {
     { month: "January", desktop: 186, mobile: 80 },
     { month: "Fevereiro", desktop: 305, mobile: 200 },
     { month: "Março", desktop: 237, mobile: 120 },
-    { month: "Abril", desktop: 73, mobile: 190 },
-    { month: "Maio", desktop: 209, mobile: 130 },
-    { month: "Junho", desktop: 214, mobile: 140 },
+    // { month: "Abril", desktop: 73, mobile: 190 },
+    // { month: "Maio", desktop: 209, mobile: 130 },
+    // { month: "Junho", desktop: 214, mobile: 140 },
   ];
 
   const chartConfig = {
